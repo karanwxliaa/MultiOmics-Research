1c338db1b059c5cb6c6217b8ae95952ab6a3c7d6
Jaskaran Singh Walia <95328038+karanwxliaa@users.noreply.github.com>
2023-06-16 00:15:32 +0530

    Add files via upload

---
+++ concatinated_arch/STAGATE/clustering.r
@@ -0,0 +1,26 @@
+# Read the data file
+data <- read.csv("C:\\Users\\KARAN\\Desktop\\MultiOmics-Research\\arch1\\SPOTS Landau paper dataset\\protein\\data.csv", header = FALSE)
+# Load the mclust package
+library(mclust)
+
+# Rest of your code here...
+mclust_R <- function(data, num_cluster=4, modelNames = "EEE", used_obsm = "test", random_seed = 2020){ # nolint
+  set.seed(random_seed)
+  library(mclust)
+  
+  # Set the random seed
+  set.seed(random_seed)
+  # Run the Mclust function
+  mclust_res <- Mclust(data, G = num_cluster, modelNames = modelNames)
+  # Get the cluster assignments
+  print(mclust_res)
+  cluster_labels <- mclust_res$classification
+  return(cluster_labels)
+}
+
+# Call the mclust_R function with the data
+result <- mclust_R(data,num_cluster=4, modelNames = "EEE", used_obsm = "test", random_seed = 2020) # nolint
+result <- unname(result)
+# Write the result to a file
+write.csv(result, "C:\\Users\\KARAN\\Desktop\\MultiOmics-Research\\arch1\\SPOTS Landau paper dataset\\protein\\results.csv", row.names = FALSE) # nolint
+
