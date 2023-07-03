2fdf73b3a35c3f62856e882f0f99c703d185d30e
Jaskaran Singh Walia <95328038+karanwxliaa@users.noreply.github.com>
2023-07-04 01:29:27 +0530

    Add files via upload

---
+++ proposed_model/protein/clustering.r
@@ -0,0 +1,27 @@
+# Read the data file
+data <- read.csv("C:\\Users\\KARAN\\Desktop\\MultiOmics-Research\\arch2\\Mouse_Thymus\\data.csv", header = FALSE)
+# Load the mclust package
+library(mclust)
+
+# Rest of your code here...
+mclust_R <- function(data, num_cluster=8, modelNames = "EEE", random_seed = 2020){ # nolint
+  set.seed(random_seed)
+  library(mclust)
+  
+  # Set the random seed
+  set.seed(random_seed)
+  # Run the Mclust function
+  mclust_res <- Mclust(data, G = num_cluster, modelNames = modelNames)
+  # Get the cluster assignments
+  print(mclust_res)
+  cluster_labels <- mclust_res$classification
+  return(cluster_labels)
+}
+
+# Call the mclust_R function with the data
+result <- mclust_R(data,num_cluster=8, modelNames = "EEE",  random_seed = 2020) # nolint
+result <- unname(result)
+
+# Write the result to a file
+write.csv(result, "C:\\Users\\KARAN\\Desktop\\MultiOmics-Research\\arch2\\Mouse_Thymus\\results.csv", row.names = FALSE) # nolint
+
